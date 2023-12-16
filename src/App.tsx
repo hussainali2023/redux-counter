f8cf9f9ea07fc07aa022381a35be24f5d0822503
Hussain <iamhussainali2@gmail.com>
2023-12-16 11:44:41 +0530

    now the counter works properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { RootState } from './redux/store'
 import './App.css'
-import { useSelector} from "react-redux"
+import { useDispatch, useSelector} from "react-redux"
+import { decrement, increment } from './features/counter/counterSlice'
 
 function App() {
 
   const count = useSelector((state:RootState) =>state.counter.count)
-  // const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
   return (
     <>
-    <button>Increment</button>
+    <button onClick={() => dispatch(increment())}>Increment</button>
     <span>{count}</span>
-    <button>Decrement</button>
+    <button onClick={() => dispatch(decrement())}>Decrement</button>
         
     </>
   )
